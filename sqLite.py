406bb383e2963b76323ddb320a7fb3f5bb6fc7c1
JennyFomin <jennifer.fomin@outlook.de>
2024-02-07 16:28:47 +0100

    adapted scripts to insert more values into the database

--- sqLite.py
+++ sqLite.py
@@ -4,7 +4,7 @@ def execute_sql_script(script_path):
     with open(script_path, 'r') as script_file:
         sql_script = script_file.read()
     # SQLite-Datenbankverbindung herstellen (wenn die Datei nicht existiert, wird sie erstellt)
-    db_connection = sqlite3.connect("energy_data.db")
+    db_connection = sqlite3.connect("smart_home_data.db")
 
     # SQLite-Cursor erstellen
     cursor = db_connection.cursor()
