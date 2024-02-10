b288a8519d48504959460527a6f76e01d05157b5
JennyFomin <jennifer.fomin@outlook.de>
2024-02-10 12:45:40 +0100

    added script to use mongoDB & updated readme instructions

--- README.md
+++ README.md
@@ -2,11 +2,19 @@
 
 ## memory monitoring
 pip install psutil
+pip install pymongo
+pip install paho-mqtt
 
 ## Mosquitto installation
 sudo add-apt-repository ppa:mosquitto-dev/mosquitto-ppa
 
 sudo apt install mosquitto mosquitto-clients
+
+## MongoDB installation
+https://www.mongodb.com/docs/manual/administration/install-community/
+
+# MongoDB Compass installation
+https://www.mongodb.com/try/download/compass
 
 ## VSCode extensions
 - SQLite Viewer 
@@ -24,16 +32,20 @@ mosquitto_sub -h localhost -t "#" -v
 ## Test publisher (message should to all subscriber -> console and DB)
 mosquitto_pub -h localhost -t "test_topic" -m 'Hello, Mosquitto!'
 
-## create database table
-python3 sqLite.py
+## run database bridge script
+python3 mongoDB-bridge.py
 
-## start bridge between mqtt broker and sqlite db
-python3 sqLite-bridge.py 
+## connect mongoDB compass to database
+URI: mongodb://localhost:27017
 
 ## start device simulation
 python3 LightSimulation.py
+
 
 # STOP SIMULATION
 
 ## stop Mosquitto
 sudo systemctl stop mosquitto
+
+## stop MongoDB
+sudo systemctl stop mongod
---
+++ mongoDB-bridge.py
@@ -0,0 +1,62 @@
+import paho.mqtt.client as mqtt
+import pymongo
+import json
+from datetime import datetime
+
+# MongoDB-Verbindungseinstellungen
+mongo_host = 'localhost'
+mongo_port = 27017
+mongo_db_name = 'smart_home_db'
+
+# MQTT-Einstellungen
+mqtt_broker = "localhost"
+mqtt_port = 1883
+mqtt_topic_energy_data = "smart_home/energy_data"  # Erstes Topic
+mqtt_topic_simulation_data = "smart_home/simulation_data"  # Neues Topic
+
+def connect_to_mongodb():
+    # Verbindung zu MongoDB herstellen
+    client = pymongo.MongoClient(mongo_host, mongo_port)
+    db = client[mongo_db_name]
+    return db
+
+# MQTT Callbacks
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
+    # Abonnieren beider Topics
+    client.subscribe([(mqtt_topic_energy_data, 0), (mqtt_topic_simulation_data, 0)])
+
+def on_message(client, userdata, msg):
+    # Verbindung zur MongoDB-Datenbank herstellen
+    db = connect_to_mongodb()
+
+    message = json.loads(msg.payload)
+    print(f"Received message: {message} on topic {msg.topic}")
+
+    # Zeitstempel zum Nachrichtenobjekt hinzufügen
+    message['timestamp'] = datetime.now()
+
+    # Unterscheidung, von welchem Topic die Nachricht stammt
+    if msg.topic == mqtt_topic_energy_data:
+        # Speichern in der 'energy_data' Kollektion
+        collection = db['energy_data']
+    elif msg.topic == mqtt_topic_simulation_data:
+        # Speichern in der 'simulation_data' Kollektion
+        collection = db['simulation_data']
+
+    # Nachricht in der entsprechenden MongoDB-Kollektion speichern
+    result = collection.insert_one(message)
+    print(f"Daten erfolgreich in {collection.name} gespeichert, Dokument-ID: {result.inserted_id}")
+
+def start_mqtt_client():
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+    client.connect(mqtt_broker, mqtt_port, 60)
+
+    # Blocking call - auf Nachrichten warten
+    client.loop_forever()
+
+if __name__ == '__main__':
+    start_mqtt_client()
