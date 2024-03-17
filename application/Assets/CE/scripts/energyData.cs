25dccb5e5a5593df21edba643158631d45a45865
guilhem6 <91888888+guilhem6@users.noreply.github.com>
2024-03-17 22:44:57 +0100

    Delete application/Assets/CE directory

--- application/Assets/CE/scripts/energyData.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-[Serializable]
-public class EnergyData
-{
-    public float time_of_day;
-    public float total_light_intensity;
-    public float total_energy_consumption;
-}
--- application/Assets/CE/scripts/simulationData.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-[Serializable]
-public class SimulationData
-{
-    public float time_of_simulation;
-    public float total_energy_consumption;
-}
