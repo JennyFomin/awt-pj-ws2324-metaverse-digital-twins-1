bc9c8007d8e0e99766f59970fafeff10028c0c79
JennyFomin <jennifer.fomin@outlook.de>
2024-03-17 12:19:06 +0100

    Deleted database files for sqLite and redis as mongoDB was the final choice and the files are not needed anymore

--- storage/scripts/insert_data_to_sqLite.sql
+++
@@ -1,9 +0,0 @@
--- -----------------
--- NOT USED 
---
--- SQL statement to insert given data in sqlite database
--- 
--- -----------------
-
-INSERT INTO smart_home_data (time_of_day, total_light_intensity, total_energy_consumption)
-VALUES (?, ?, ?);
