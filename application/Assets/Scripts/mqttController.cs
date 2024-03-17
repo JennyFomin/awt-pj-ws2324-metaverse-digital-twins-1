2c1ea3ba568e67c8f86c38e3b025971730d8e607
guilhem6 <91888888+guilhem6@users.noreply.github.com>
2024-03-17 22:50:52 +0100

    Update mqttController.cs

--- application/Assets/Scripts/mqttController.cs
+++ application/Assets/Scripts/mqttController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-
 [System.Serializable]
 public class MessageData
 {
@@ -13,44 +12,41 @@ public class MessageData
 
 public class mqttController : MonoBehaviour
 {
-
-
-  public string nameController = "Controller 1";
-  public string tagOfTheMQTTReceiver="MQTT_Receiver";
-  public mqttReceiver _eventSender;
+    public string nameController = "Controller 1";
+    public string tagOfTheMQTTReceiver = "MQTT_Receiver";
+    public mqttReceiver _eventSender;
 
     public float time_of_day;
     public float total_light_intensity;
     public GameObject clockObject;
     private TimeManager timeManager;
 
-  void Start()
-  {
-    //textMeshPro = this.GetComponent<TextMeshPro>();
+    void Start()
+    {
+        //textMeshPro = this.GetComponent<TextMeshPro>();
 
-    _eventSender=GameObject.FindGameObjectsWithTag(tagOfTheMQTTReceiver)[0].gameObject.GetComponent<mqttReceiver>();
-    _eventSender.OnMessageArrived += OnMessageArrivedHandler;
-    timeManager = clockObject.GetComponent<TimeManager>();
-  }
+        _eventSender = GameObject.FindGameObjectsWithTag(tagOfTheMQTTReceiver)[0].gameObject.GetComponent<mqttReceiver>();
+        _eventSender.OnMessageArrived += OnMessageArrivedHandler;
+        timeManager = clockObject.GetComponent<TimeManager>();
+    }
 
-  private void OnMessageArrivedHandler(string newMsg)
-{
-      
-        // Désérialiser la chaîne JSON en un objet MessageData
+    private void OnMessageArrivedHandler(string newMsg)
+    {
+        // Deserialize the JSON string into a MessageData object
         MessageData data = JsonUtility.FromJson<MessageData>(newMsg);
 
-        // Récupérer les valeurs de time_of_day et light_intensity de l'objet MessageData
-        if(data.time_of_day!=0 && data.total_light_intensity!=null)
+        // Retrieve the values of time_of_day and light_intensity from the MessageData object
+        if (data.time_of_day != 0 && data.total_light_intensity != null)
         {
-          Debug.Log(data.time_of_day);
-        time_of_day = data.time_of_day;
-        total_light_intensity = data.total_light_intensity;
+            Debug.Log(data.time_of_day);
+            time_of_day = data.time_of_day;
+            total_light_intensity = data.total_light_intensity;
 
-        // Modifier la variable simulatedHour dans le script TimeManager de l'objet Clock
-        if (timeManager != null)
-        {
-            timeManager.simulatedHour = time_of_day;
-        }
+            // Modify the simulatedHour variable in the TimeManager script of the Clock object
+            if (timeManager != null)
+            {
+                timeManager.simulatedHour = time_of_day;
+            }
         }
     }
 }
